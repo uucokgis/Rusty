395c37f9ae9bb7c1734fce219fadfef8f3ae5793
Umut Ucok <ucok.umut@gmail.com>
2020-04-02 13:56:29 +0300

    formatted

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,2 @@
 fn main () {
-    let x = vec![1,2,3];
-    println!("Cant use x here : {:?}", x);
-    let equal_to_x = move |z| {z == x};
-    let y = vec![1,2,3];
-
-    println!("{:?}", equal_to_x(y));
 }
