3fd7aed12459f7817f129cbf7137d1b6e7c7b913
Umut Ucok <ucok.umut@gmail.com>
2020-04-02 13:56:09 +0300

    bug fixed around closure environments

--- src/closures/advancedv3.rs
+++ src/closures/advancedv3.rs
@@ -1,8 +1,9 @@
 // closures also can take pointer of variable or ownership or mutable pointer.
 fn move_sample() {
     let x = vec![1,2,3];
-    let equal_to_x = move |z: Vec<u32>| {z == x};
     println!("Cant use x here : {:?}", x);
+    let equal_to_x = move |z| {z == x};
     let y = vec![1,2,3];
-//    assert_eq!(equal_to_x(y));
+
+    println!("{:?}", equal_to_x(y));
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main () {
+    let x = vec![1,2,3];
+    println!("Cant use x here : {:?}", x);
+    let equal_to_x = move |z| {z == x};
+    let y = vec![1,2,3];
 
+    println!("{:?}", equal_to_x(y));
 }
