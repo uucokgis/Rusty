7c0ffe9524772039ad59e992481d4925624a4626
umut ucok <ucok.umut@gmail.com>
2020-06-16 20:36:09 +0300

    ch 17-03 *- Design Patterns

---
+++ src/gui/src/lib.rs
@@ -0,0 +1,44 @@
+// testing area, not working. but you got the idea.
+
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        // code to actually draw a button
+    }
+}
+
+pub struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // code to actually draw a select box
+    }
+}
+
+pub struct Screen<T: Draw> {
+    pub components: Vec<T>,
+}
+
+impl<T> Screen<T>
+    where
+        T: Draw,
+{
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
