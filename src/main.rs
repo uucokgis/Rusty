1da7961fd2393f6819de1bf8aa59f3080fadede6
umut ucok <ucok.umut@gmail.com>
2020-06-17 16:41:59 +0300

    ch 17-03 *- Singleton Pattern

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
 // our gui
 
+use post::Post;
+
 fn main () {
+    let mut mypost = Post::new();
+
+    mypost.add_text("I ate a salad for lunch today");
+    assert_eq!("", mypost.content());
+
+    mypost.request_review();
+    assert_eq!("", mypost.content());
+
+    mypost.approve();
+    assert_eq!("I ate a salad for lunch today", mypost.content());
+
 }
