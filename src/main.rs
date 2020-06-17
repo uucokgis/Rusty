1e9da4f5c9c9a5d1bbcb11620eb6a48abe0b6925
umut ucok <ucok.umut@gmail.com>
2020-06-17 17:33:20 +0300

    ch 17-03 *- Singleton Pattern
    I also added some mechanism to check state

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,4 @@
-use post::Post;
 
 fn main () {
-    let mut p = Post::new();
-    p.add_text("my post");
-    println!("state of the post : {}", p.print_state());
 
-    p.request_review();
-    println!("state of the post : {}", p.print_state());
-
-    p.approve();
-    println!("state of the post : {}", p.print_state());
 }
