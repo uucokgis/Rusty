01d0249b45c9e48aed42ac06fa2258b0d2e673b4
umut ucok <ucok.umut@gmail.com>
2020-06-18 15:09:44 +0300

    ch 19-00  -> Important Chapter !

---
+++ src/patterns_syntax/basics.rs
@@ -0,0 +1,113 @@
+fn at_bindings() {
+    enum Message {
+        Hello { id: i32 },
+    }
+
+    let msg = Message::Hello { id: 5 };
+
+    match msg {
+        Message::Hello {
+            id: id_variable @ 3..=7,
+        } => println!("Found an id in range: {}", id_variable),
+        Message::Hello { id: 10..=12 } => {
+            println!("Found an id in another range")
+        }
+        Message::Hello { id } => println!("Found some other id: {}", id),
+    }
+}
+
+fn or_fn() {
+    let x = 4;
+    let y = false;
+
+    match x {
+        4 | 5 | 6 if y => println!("yes"),
+        _ => println!("no"),
+    }
+}
+
+fn list_seperator_basic() {
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, _, third, _, fifth) => {
+            println!("Some numbers: {}, {}, {}", first, third, fifth)
+        }
+    }
+}
+
+fn list_seperator_middle() {
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (.., second, ..) => {
+            println!("Some numbers: {}", second)
+        }
+    }
+}
+
+fn list_seperator_first_end() {
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, .., last) => {
+            println!("Some numbers: {}, {}", first, last);
+        }
+    }
+}
+
+fn match_inside() {
+    struct Point {
+        x: i32,
+        y: i32,
+        z: i32,
+    }
+
+    let origin = Point { x: 0, y: 0, z: 0 };
+
+    match origin {
+        Point { x, .. } => println!("x is {}", x),
+    }
+}
+
+fn enum_style() {
+    enum Color {
+        Rgb(i32, i32, i32),
+        Hsv(i32, i32, i32),
+    }
+
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(Color),
+    }
+
+    fn inside() {
+        let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
+
+        match msg {
+            Message::ChangeColor(Color::Rgb(r, g, b)) => println!(
+                "Change the color to red {}, green {}, and blue {}",
+                r, g, b
+            ),
+            Message::ChangeColor(Color::Hsv(h, s, v)) => println!(
+                "Change the color to hue {}, saturation {}, and value {}",
+                h, s, v
+            ),
+            _ => (),
+        }
+    }
+
+    inside();
+}
+
+fn letter_seperator () {
+    let x = 'c';
+
+    match x {
+        'a'..='j' => println!("early ASCII letter"),
+        'k'..='z' => println!("late ASCII letter"),
+        _ => println!("something else"),
+    }
+}
